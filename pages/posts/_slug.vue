7e30c8244ab60250e10afdb0b17903c469adc158
hiurequeiroz <hiure@riseup.net>
2022-02-02 09:21:27 -0300

    add back to blog and projetcs

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,5 +1,18 @@
 <template>
   <article>
+
+    <span class="relative top-3 left-2">
+    <NuxtLink to="/blog">
+          <div class="flex flex-row gap-1">
+            <img
+              src="~/assets/back.svg"
+              alt="iconeBack"
+              class="w-35px h-35px cursor-pointer"
+            />
+          </div>
+    </NuxtLink>
+    </span>
+
     <h2 class="text-3xl text-center py-10px">{{ page.titulo }}</h2>
     <div class="w-90vw m-auto py-10px">
       <nuxt-content :document="page" />
