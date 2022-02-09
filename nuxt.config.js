26ae9c88e04f06069e10704bf7046acd221a2b88
hiurequeiroz <hiure@riseup.net>
2022-02-09 19:07:48 -0300

    resolve problema de es module

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,47 +48,9 @@ export default {
     "@nuxtjs/svg",
   ],
   components: true,
-  build: {
-    extend(config, ctx) {}, // blah blah
-    transpile: [
-      'unist-util-find-before',
-      "hast-util-select",
-      "direction",
-      "comma-separated-tokens",
-      "bcp-47-match",
-      "unist-util-is",
-      "unist-util-visit",
-      "hast-util-is-element",
-      "hast-util-has-property",
-      "hast-util-whitespace",
-      "space-separated-tokens",
-      "property-information",
-      "zwitch",
-      "css-selector-parser",
-      "hast-util-to-string",
-      "not",
-      "nth-check",
-      'is-absolute-url',
-      'extend',
-      'rehype-sort-attribute-values',
-      'rehype-sort-attributes',
-      'rehype-raw',
-      'remark-squeeze-paragraphs',
-      'remark-slug',
-      'remark-autolink-headings',
-      'remark-external-links',
-      'remark-footnotes',
-      'remark-gfm'
-    ]
-  },
+ 
   server: {
     host: "0.0.0.0",
   },
-  content: {
-    markdown: {
-      rehypePlugins: [
-        'rehype-external-links'
-      ]
-    }
-  },
+
 };
